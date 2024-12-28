b4d1e89fab133db380552991698b2040b38e128a
Juan Fuente <127140423+Juan-Fuente-T@users.noreply.github.com>
2024-12-28 13:03:30 +0100

    Feat: Spring Jokes Spring App funcionando

---
+++ src/test/java/com/juanfuente/spring_boot_jokes_app/SpringBootJokesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juanfuente.spring_boot_jokes_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJokesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
